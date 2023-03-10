2b56d35b5cb41f86706807e494071e7a6a7d59da
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2023-03-10 12:59:06 +0200

    Basic Hello World Achieved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Hello, World!</h1>',
 })
-export class AppComponent {
-  title = 'hello-world';
-}
+export class AppComponent {}
+
